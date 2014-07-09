21d2633000c5ea8f2bed9ac2c876fc3143264a25
msalli <alli.cernoch@gmail.com>
2014-07-08 22:50:26 -0700

    Finished Temp Convert project

---
+++ tempConvert.js
@@ -0,0 +1,15 @@
+//tempConvert.js
+//Out a temperature conversion.
+//Convert it to fahrenheit and output "NN°C is NN°F".
+
+var celsius = 45;
+
+var fahrenheit = celsius * 9 / 5 + 32;
+
+console.log(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit!");
+
+var fahrenheit2 = 78;
+
+var cel2 = (78 -2) * 5 / 9
+
+console.log(fahrenheit2 + " degrees fahrenheit is " + cel2 + " degrees celsius.");
